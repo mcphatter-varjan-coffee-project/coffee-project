6a084cdb9465a1d08d835d7e3cb358dc4439e8e3
timvarjan <127249482+timvarjan@users.noreply.github.com>
2023-04-17 12:39:44 -0300

    pulling from main

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 function renderCoffee(coffee) {
     var html = '<div class="coffee w-50 d-flex flex-nowrap my-2">';
     // html += '<td>' + coffee.id + '</td>';
-    html += '<h4 class=" text-nowrap mb-0 ">' + coffee.name +  '</h4>';
+    html += '<h4 class=" text-nowrap mb-0 ">' + coffee.name + '</h4>';
     html += '<p class="ms-1 mt-1" style="color: slategray">' + coffee.roast + '</p>';
     html += '</div>';
 
@@ -14,7 +14,7 @@ function renderCoffee(coffee) {
 // displays all coffees
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = 0; i < coffees.length; i++) {
+    for (var i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -25,7 +25,7 @@ function updateCoffees(input) {
     input.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
+    coffees.forEach(function (coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
@@ -62,8 +62,8 @@ submitButton.addEventListener('click', updateCoffees);
 
 //Add coffees to list
 
-function addCoffees (input) {
-    var addID = coffees.length+1;
+function addCoffees(input) {
+    var addID = coffees.length + 1;
     var addName = inputName.value.toString();
     var addRoast = inputRoast.value.toString();
     input = {id: addID, name: addName, roast: addRoast};
@@ -71,6 +71,7 @@ function addCoffees (input) {
     console.log(coffees);
     coffeeList.innerHTML = renderCoffees(coffees);
 }
+
 var addCoffeeBtn = document.getElementById('add-coffee');
 
 
@@ -79,31 +80,58 @@ var coffeeList = document.querySelector('#coffees');
 // coffeeList.innerHTML = renderCoffees(coffees);
 
 //Live search
-function searchCoffees() {
-    var searchRoast = searchBox.value.toUpperCase();
-    var filteredCoffees = [];
-    console.log(searchRoast);
-    coffees.forEach(function(coffee) {
-        if (coffee.name.toUpperCase().includes(searchRoast)) {
-            filteredCoffees.push(coffee);
-            console.log(filteredCoffees);
-        }
-    });
-    coffeeList.innerHTML = renderCoffees(filteredCoffees);
-}
+const search = document.getElementById("name");
+const results = document.getElementById("roast");
+let search_term = "";
+
+const showList = () => {
+    results.innerHTML = "";
+    data
+        .filter((item) => {
+            return (
+                item.name.toLowerCase().includes(search_term) ||
+                item.roast.toLowerCase().includes(search_term)
+            );
+        })
+        .forEach((e) => {
+            const li = document.createElement("li");
+            li.innerHTML = `<i>Name:</i> ${e.name}  || <i>Roast:</i> ${e.roast}`;
+            results.appendChild(li);
+        });
+};
+
+showList();
+
+search.addEventListener("input", (event) => {
+    search_term = event.target.value.toLowerCase();
+    showList();
+});
+
+// function searchCoffees() {
+//     var searchRoast = searchBox.value.toUpperCase();
+//     var filteredCoffees = [];
+//     console.log(searchRoast);
+//     coffees.forEach(function(coffee) {
+//         if (coffee.name.toUpperCase().includes(searchRoast)) {
+//             filteredCoffees.push(coffee);
+//             console.log(filteredCoffees);
+//         }
+//     });
+//     coffeeList.innerHTML = renderCoffees(filteredCoffees);
+// }
 // console.log(renderCoffees(coffees));
 
 // JS CODE FOR DARK MODE
-var bodyColor  = document.getElementById('body-color');
+var bodyColor = document.getElementById('body-color');
 var modeSwitch = document.getElementById('flexSwitchCheckDefault');
 var modeText = document.getElementsByClassName('form-check-label')
-modeSwitch.addEventListener("click", function (){
+modeSwitch.addEventListener("click", function () {
     var onOff = modeSwitch.checked;
-    if (onOff === true){
+    if (onOff === true) {
         bodyColor.style.backgroundColor = 'black';
         bodyColor.style.color = 'white';
         modeText[0].innerText = 'Night '
-    } else if (onOff === false){
+    } else if (onOff === false) {
         bodyColor.style.backgroundColor = 'white';
         bodyColor.style.color = 'black';
         modeText[0].innerText = 'Day ';
